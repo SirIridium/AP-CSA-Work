bd54e671fd7c9d9d7a97f7da7e5c2c7f4b8a9e1e
SirIridium <127507341+SirIridium@users.noreply.github.com>
2023-10-16 13:05:40 -0400

    Instant looping

--- Something.java
+++ Something.java
@@ -1,10 +1,11 @@
+import javax.sound.midi.SysexMessage;
 
 public class Something {
     public static void main(String[] args) {
         System.out.println("");
-        System.out.println("Hello World");
-          System.out.println("Hello Felix");
+        System.out.println("Hello Felix");
         
+
         
 
 
