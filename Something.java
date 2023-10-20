e1d705d7efd049b265c33e2f8b8b332955a76e9e
SirIridium <127507341+SirIridium@users.noreply.github.com>
2023-10-20 12:29:48 -0400

    IT WORKS

--- Something.java
+++ Something.java
@@ -1,12 +1,12 @@
 import java.text.AttributedCharacterIterator.Attribute;
-import javax.sound.midi.SysexMessage;
-
 
 
 public class Something {
     public static void main(String[] args) {
-        Content
-        Content.printYear();
+        YearPrinter HI = new YearPrinter(); 
+        
+        
+        HI.printYear(100);
         
             
     }
--- YearPrinter.java
+++ YearPrinter.java
@@ -1,5 +1,16 @@
-public class Content {
-
-    private int year;
-    private String title; 
+public class YearPrinter {
+    
+    //This is a test function
+    public void printYear (int whatYear) {
+        int year = whatYear;
+        int notyear = 0;
+        for (notyear=0; notyear < year; notyear++) {
+            
+            System.out.println("The year is not Actually " + notyear);
+            System.out.println("");
+            System.out.println("");
+        }
+    System.out.println("The year is Actually " + whatYear);
+    System.out.println(" ");
+    }
 }
