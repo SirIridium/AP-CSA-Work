80021e31f653bec747428c9aef8542e60a68eee0
SirIridium <127507341+SirIridium@users.noreply.github.com>
2023-10-20 12:18:22 -0400

    I made a new file

---
+++ Content.java
@@ -0,0 +1,14 @@
+public class Content {
+    
+    //This is a test function
+    public void printYear () {
+        int year = 5;
+        int notyear = 1;
+        for (notyear=0; notyear < year; notyear++) {
+            
+            System.out.println("The not year is " + notyear);
+            System.out.println("");
+            System.out.println("");
+        }
+    }
+}
