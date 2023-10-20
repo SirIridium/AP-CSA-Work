02c478369f1d88e40dc018aa9e0297632c2c9785
SirIridium <127507341+SirIridium@users.noreply.github.com>
2023-10-20 12:18:12 -0400

    modified

--- Something.java
+++ Something.java
@@ -5,24 +5,10 @@ import javax.sound.midi.SysexMessage;
 
 public class Something {
     public static void main(String[] args) {
-        Content Donut = new Content(); 
         
-        int year = 10000;
-        int notyear = 1;
-        for (notyear=0; notyear < year; notyear++) {
-            
-            System.out.println("The not year is " + notyear);
-            System.out.println("");
-            System.out.println("");
-            
-}
+        Content.printYear();
         
+            
+    }
 
-
-
-
-
-
-
-    } 
-}
+} 
