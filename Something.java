32c5c7fbaef82a36401f309ac35b910133b0e34d
SirIridium <127507341+SirIridium@users.noreply.github.com>
2023-10-20 12:20:13 -0400

    renamed to avoid keywords

--- Something.java
+++ Something.java
@@ -5,7 +5,7 @@ import javax.sound.midi.SysexMessage;
 
 public class Something {
     public static void main(String[] args) {
-        
+        Content
         Content.printYear();
         
             
